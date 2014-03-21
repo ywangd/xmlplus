502166cdc9aeaa780e6c9cab98f787d61e3a0303
Yang Wang <y.wang@bom.gov.au>
2014-03-21 16:19:27 +1100

    A rough journey ahead for a better future. First clojure project. Working on XML manipulation.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to mdgen
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/mdgen/core_test.clj
@@ -0,0 +1,7 @@
+(ns mdgen.core-test
+  (:require [clojure.test :refer :all]
+            [mdgen.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
