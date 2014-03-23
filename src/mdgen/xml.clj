b019eb970494be948650cf2f7034f17da8b560e1
Yang Wang <ywangd@gmail.com>
2014-03-23 14:37:02 +1100

    Some test and play.

--- src/mdgen/xml.clj
+++ src/mdgen/xml.clj
@@ -2,9 +2,10 @@
  (:require [clojure.zip :as zip]
            [clojure.xml :as xml]
            [clojure.java.io :as io]
-           [clojure.data.zip.xml :as zf]))
+           [clojure.data.zip :as zf]
+           [clojure.data.zip.xml :as zfx]))
 
-(defn read
+(defn read->root
   "Read a given xml file and return the root node"
   [fname]
   (with-open [ins (io/input-stream fname)]
@@ -14,8 +15,8 @@
 (defn get-node
   "Return a lazy seq of matching nodes"
   ([node tag]
-  (for [x (xml-seq node) :when (= tag (:tag x))]
-    x))
+	  (for [x (xml-seq node) :when (= tag (:tag x))]
+	    x))
   ([node tag val]
     (for [x (xml-seq node) 
           :when (and (= tag (:tag x)) (= val (-> x :content first)))]
@@ -24,7 +25,7 @@
 (defn query
   "Similar to xml-> but takes a node instead of loc. Returns a lazy seq."
   [node & args]
-  (apply zf/xml-> (zip/xml-zip node) args))
+  (apply zfx/xml-> (zip/xml-zip node) args))
 
 (defn query1
   "Take the first element from the query result"
@@ -34,4 +35,19 @@
 (defn edit
   [root & args]
   (zip/root (zip/edit (apply query1 root args) #(assoc % :content ["Changed"])))
+)
+
+(defmacro x->
+  [loc & args]
+  `(loop [res# ~loc preds# ~args]
+     (let [pred# ~(first '~preds#)]
+       (print 'pred#)
+     ;(if (= nil 'pred#) res#)
+     ;(if (= (type pred#) ~java.lang.Long)
+      ;(recur (first res#) (rest preds#))
+      ;(recur (apply #(zfx/xml-> % pred#) res#) (rest preds#))
+      ;res#
+      ;)
+     )
+   )
 )
