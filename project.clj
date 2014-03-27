5ade2bae7966d718774498c27bc98316e2b8904c
Yang Wang <ywangd@gmail.com>
2014-03-27 23:11:21 +1100

    Added instaparser as a dependancy. Going to give it a spin for fun.

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.zip "0.1.1"]
-                 [org.clojure/data.xml "0.0.7"]])
+                 [org.clojure/data.xml "0.0.7"]
+                 [instaparse "1.3.0"]])
