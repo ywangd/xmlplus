efd67b9aa5b56d18a27466e1341293dfab4335c1
Yang Wang <y.wang@bom.gov.au>
2014-05-17 10:30:25 +1000

    Refactor the project to narrow down its focus.

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-# mdgen
+# XML Plus
 
-Create and manipulate XML metadata files
+Add more functionalities to Clojure's builtin clojure.xml and contrib package
+clojure.data.zip.xml. Most notably add the support for using integers to select
+a single element in a multiple match.
+
 
 ## Usage
 
--- project.clj
+++ project.clj
@@ -1,16 +1,8 @@
 (defproject mdgen "0.1.0-SNAPSHOT"
-  :description "XML Metadata manipulation tool"
-  :url "http://example.com/FIXME"
+  :description "Work with XML zipper with added functionalities"
+  :url "https://github.com/ywangd/xmlplus"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.zip "0.1.1"]
-                 [org.clojure/data.xml "0.0.7"]
-                 [org.clojure/data.json "0.2.4"]
-                 [clojurewerkz/propertied "1.1.0"]
-                 [clj-time "0.6.0"]
-                 [org.clojure/java.jdbc "0.3.3"]
-                 [local/ojdbc6 "11.2.0.3"]
-                 [lein-light-nrepl "0.0.17"]
-                 [instaparse "1.3.0"]]
-  :repl-options {:nrepl-middleware [lighttable.nrepl.handler/lighttable-ops]})
+                 [org.clojure/data.xml "0.0.7"]])
